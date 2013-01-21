64641ec0fb2f04d541e75e92199568d15a50b479
tomcynicholls <tom.cy.nicholls@gmail.com>
2013-01-21 12:54:06 +0000

    View Xml Writer added
    App takes input text and creates xml document
    Next: Stream document into socket - stream to server
    Server takes and saves xml locally

--- JavaCode/App/gen/com/thirdyearproject/app/R.java
+++ JavaCode/App/gen/com/thirdyearproject/app/R.java
@@ -14,17 +14,31 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int edit_message=0x7f050000;
+        public static final int edit_message=0x7f060004;
+        public static final int innerlin=0x7f060000;
+        public static final int innerrel=0x7f060002;
+        public static final int menu_settings=0x7f060005;
+        public static final int show_message=0x7f060001;
+        public static final int xmlresult=0x7f060003;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int activity_display_message=0x7f030000;
+        public static final int activity_view_xml_writer=0x7f030001;
+        public static final int main=0x7f030002;
+    }
+    public static final class menu {
+        public static final int activity_display_message=0x7f050000;
+        public static final int activity_view_xml_writer=0x7f050001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int button_send=0x7f040002;
         public static final int edit_message=0x7f040001;
+        public static final int hello_world=0x7f040006;
+        public static final int holder=0x7f040007;
         public static final int menu_settings=0x7f040003;
         public static final int title_activity_display_message=0x7f040005;
         public static final int title_activity_main=0x7f040004;
+        public static final int title_activity_view_xml_writer=0x7f040008;
     }
 }
